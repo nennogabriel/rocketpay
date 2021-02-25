c4f05a295c0dc8ea0f4299ce23c243d36e8001f0
Pedro Moreno <nennogabriel@gmail.com>
2021-02-24 22:36:29 -0300

    Aula 3

---
+++ lib/rocketpay_web/views/accounts_view.ex
@@ -0,0 +1,10 @@
+defmodule RocketpayWeb.AccountsView do
+  # alias Rocketpay.{Account, User}
+
+  def render("update.json", account), do: account
+  #   %{
+  #     message: "Deposit completed",
+  #     account: account
+  #   }
+  # end
+end
